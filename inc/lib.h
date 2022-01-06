7a9987a263351656b1e2415fdd59228197e8c9f0
Sioryn Indianna Nereyeus  Willett <s4392620@student.uq.edu.au>
2022-01-06 18:20:48 +1000

    Added function comment

--- inc/lib.h
+++ inc/lib.h
@@ -9,6 +9,12 @@
 #ifndef LIBRARY_NAME_LIB_H
 #define LIBRARY_NAME_LIB_H
 
+/**
+ * libs a number
+ * 
+ * @param   input  the number to lib 
+ * @return  The result of the libbing
+ */
 int lib(int input);
 
 #endif //LIBRARY_NAME_LIB_H
