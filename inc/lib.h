0a14f756fd99ce797af2c0301f2bd6a3c28c746b
Sioryn Indianna Nereyeus  Willett <s4392620@student.uq.edu.au>
2022-01-06 18:15:50 +1000

    Added IDEA settings
    added example library header, and source
    
    added example test program
    
    added example CMakeLists.txt that compiles the library and has testing enabled

---
+++ inc/lib.h
@@ -0,0 +1,14 @@
+/**
+ ***************************************************************
+ * @file lib.h
+ * @author Sioryn Willett
+ * @date 2022-01-06
+ * @brief //TODO
+ ***************************************************************
+ */
+#ifndef LIBRARY_NAME_LIB_H
+#define LIBRARY_NAME_LIB_H
+
+int lib(int input);
+
+#endif //LIBRARY_NAME_LIB_H
---
+++ test/test.cpp
@@ -0,0 +1,15 @@
+/**
+ ***************************************************************
+ * @file test.cpp 
+ * @author Sioryn Willett
+ * @date 2022-01-06
+ * @brief //TODO
+ ***************************************************************
+ */
+extern "C" {
+#include "lib.h"
+}
+
+int main(int argc, char** argv) {
+    return (lib(3) != 6);
+}
